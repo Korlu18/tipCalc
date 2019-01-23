bd368690c7322be2b8ab39358c146fae2502e890
Korlu Kemah <kkemah14@gmail.com>
2019-01-22 23:01:31 -0500

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Korlu Kemah on 1/22/19.
+//  Copyright © 2019 Korlu Kemah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
